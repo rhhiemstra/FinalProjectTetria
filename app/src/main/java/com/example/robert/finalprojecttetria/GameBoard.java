fecbf46e8fa50eae3f357280a341ab602906cdd3
rhhiemstra <rhhiemstra@wccnet.edu>
2018-11-07 01:24:41 -0500

    Commit

---
+++ app/src/main/java/com/example/robert/finalprojecttetria/GameBoard.java
@@ -0,0 +1,13 @@
+package com.example.robert.finalprojecttetria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_board);
+    }
+}
---
+++ app/src/main/java/com/example/robert/finalprojecttetria/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.robert.finalprojecttetria;
+
+import android.content.Intent;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView title;
+    Button newGameBtn;
+    Button continueBtn;
+    Intent myIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        title = findViewById(R.id.title);
+        newGameBtn = findViewById(R.id.newGameBtn);
+        continueBtn = findViewById(R.id.continueBtn);
+        myIntent = new Intent(this, GameBoard.class);
+
+        title.setPaintFlags(title.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        newGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
+
+
+    }
+}
